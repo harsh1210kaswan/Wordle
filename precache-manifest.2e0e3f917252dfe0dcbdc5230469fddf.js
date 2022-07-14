716fd7fe68adac4ea7c3b884d260e57882cb5ba3
harsh1210kaswan <harsh_2k19ec069@dtu.ac.in>
2022-07-14 16:08:46 +0530

    Updates

---
+++ precache-manifest.2e0e3f917252dfe0dcbdc5230469fddf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f16504be3e71a159ba84",
+    "url": "/Wordle/static/css/main.5c796710.chunk.css"
+  },
+  {
+    "revision": "f16504be3e71a159ba84",
+    "url": "/Wordle/static/js/main.f16504be.chunk.js"
+  },
+  {
+    "revision": "ec2d9068c196046d8898",
+    "url": "/Wordle/static/js/1.ec2d9068.chunk.js"
+  },
+  {
+    "revision": "2bb0bc5e634d14ee189e",
+    "url": "/Wordle/static/js/runtime~main.2bb0bc5e.js"
+  },
+  {
+    "revision": "46a0b80a9c393c6cf4667268865290f7",
+    "url": "/Wordle/index.html"
+  }
+];
